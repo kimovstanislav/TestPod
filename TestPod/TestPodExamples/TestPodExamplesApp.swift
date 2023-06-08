686b0e35036c74f1b54592f5fe6049b75e45490a
skimov <kimovstanislav@gmail.com>
2023-06-08 13:58:05 +0200

    Created a framework project.

---
+++ TestPod/TestPodExamples/TestPodExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPodExamplesApp.swift
+//  TestPodExamples
+//
+//  Created by Stanislav Kimov on 08.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TestPodExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
